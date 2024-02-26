ba9d6fb783d47e476e3776e9a0e9948f7384456a
Ryan <90571282+issryan@users.noreply.github.com>
2024-02-26 12:45:16 -0500

    created temporary backend template

---
+++ server/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}, () => console.log("Connected to MongoDB"));
+
+app.listen(5000, () => console.log("Server Started"));
