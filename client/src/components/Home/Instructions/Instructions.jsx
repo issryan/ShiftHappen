e184e6b194cd891541f82fcbc56ad0d1a3249762
Ryan <90571282+issryan@users.noreply.github.com>
2024-05-04 23:54:11 -0400

    adjusted instructions section for landing page

--- client/src/components/Home/Instructions/Instructions.jsx
+++ client/src/components/Home/Instructions/Instructions.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Instructions.css';
-import teamTableImage from '../../../assets/team-table-image.png';
-
+import teamTableImage from '../../../assets/Group.svg';
 
 function Instructions() {
     return (
@@ -14,31 +13,32 @@ function Instructions() {
                 <div className="team-table-image">
                     <img src={teamTableImage} alt="Team Members Table" />
                 </div>
+
                 <ol className="instructions-list">
                     <li className="instruction-item">
                         <div className="number-circle">1</div>
-                        <div>
+                        <div className='instruction-info'>
                             <h3>Set Your Team</h3>
                             <p>Create profiles for your employees and define their weekly availability. Building your team is the first step towards streamlined scheduling.</p>
                         </div>
                     </li>
                     <li className="instruction-item">
                         <div className="number-circle">2</div>
-                        <div>
+                        <div className='instruction-info'>
                             <h3>Generate the Schedule</h3>
                             <p>With a single click, 'Shift Happens' auto-populates your schedule, smartly aligning employee availability with your staffing needs.</p>
                         </div>
                     </li>
                     <li class="instruction-item">
                         <div class="number-circle">3</div>
-                        <div>
+                        <div className='instruction-info'>
                             <h3>Fine-tune & Share</h3>
                             <p>Drag-and-drop to adjust shifts as needed. Then, effortlessly export the schedule or share it directly with your team.</p>
                         </div>
                     </li>
                 </ol>
             </div>
-        </section>
+        </section >
     );
 }
 
