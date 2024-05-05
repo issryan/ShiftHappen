525ecdfb50c6f7add85877043e92cfc2e49b8b90
Ryan <90571282+issryan@users.noreply.github.com>
2024-05-05 00:38:41 -0400

    footerCTA css update positioning

--- client/src/components/Home/FooterCTA/FooterCTA.jsx
+++ client/src/components/Home/FooterCTA/FooterCTA.jsx
@@ -6,7 +6,9 @@ function FooterCTA() {
         <section className="footer-cta">
             <h2>Ready to get started?</h2>
             <p>Take the first step towards effortless scheduling.</p>
+            <div className='btn-container'>
             <button className="cta-button">Launch Your Schedule</button>
+            </div>
         </section>
     );
 }
