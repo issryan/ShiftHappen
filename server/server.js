6a5789c66420fe7ab8a604ae3578f2f3ef166ede
Ryan <90571282+issryan@users.noreply.github.com>
2024-05-05 20:10:27 -0400

    added backend auth for user login

--- server/server.js
+++ server/server.js
@@ -3,31 +3,30 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const employeeRoutes = require('./routes/employeeRoutes'); 
-const eventRoutes = require('./routes/eventRoutes'); 
-
+const employeeRoutes = require('./routes/employeeRoutes');
+const eventRoutes = require('./routes/eventRoutes');
+const authRoutes = require('./routes/authRoutes'); 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-//ROUTES
+// ROUTES
 app.use('/api/employees', employeeRoutes);
-app.use('/api/events', eventRoutes); 
-
+app.use('/api/events', eventRoutes);
+app.use('/api/auth', authRoutes); 
 
 const PORT = process.env.PORT || 5001;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
 
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
-
-
+.then(() => {
+    console.log('MongoDB connected');
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+})
+.catch(err => console.log(err));
